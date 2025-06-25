0b1e330dcd667f6f443a04b2c0acc135bebedca9
eduardosa54 <eduardo.sa@rio.rj.gov.br>
2025-06-25 16:58:59 -0300

    refactor: Refatorar CampaignsTable por enquanto

--- src/app/(services)/contracheque/CampaignsTable.tsx
+++ src/app/(services)/contracheque/CampaignsTable.tsx
@@ -3,44 +3,29 @@ import { Badge } from "@/components/ui/badge";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
 import { STATUS_COLORS } from "@/utils/constants";
-import { Campaign } from "./page";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Campaign } from "@/utils/types";
+import { useQuery } from "@tanstack/react-query";
 import obterCampanhasProprietario from "@/api/obterCampanhas";
 
 type CampaignsTableProps = {
-  campaigns: Campaign[];
   handleSetDefaultCampaign: (campaign: Campaign) => void;
 };
 
 export const CampaignsTable = ({
-  campaigns,
   handleSetDefaultCampaign,
 }: CampaignsTableProps) => {
-  // const queryClient = useQueryClient();
 
   const query = useQuery({
     queryKey: ["campaigns"],
     queryFn: () =>
-      obterCampanhasProprietario(
-        "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJMWUFNbUUwVUZBd1hyUXJGbEgwSlNrMmtCR3FSblFiMDFWRG55a3R1UE5NIn0.eyJqdGkiOiIzNjRmODFmNy0yZTI0LTRiOGMtYmUxYS03MWU1MGMzNDg3MjkiLCJleHAiOjE3NTA3MDA2NDAsIm5iZiI6MCwiaWF0IjoxNzUwNzAwMDQwLCJpc3MiOiJodHRwczovL2F1dGgtaWRyaW9ob20uYXBwcy5yaW8uZ292LmJyL2F1dGgvcmVhbG1zL2lkcmlvX2NpZGFkYW8iLCJhdWQiOlsiYnJva2VyIiwiYWNjb3VudCJdLCJzdWIiOiI2MGZlMWE0Yi1mMmUyLTQyN2QtODFiMC1kZTVhMGM4ZDU1YmQiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJtaW5oYXNlbXByZXNhcy5hcHBzLnJpby5nb3YuYnIiLCJub25jZSI6ImJiMTVlMmEyLTE3YmMtNDM3NS05MTBkLTBjNzhlMTAzZTA3MyIsImF1dGhfdGltZSI6MTc1MDY5ODI0MCwic2Vzc2lvbl9zdGF0ZSI6IjUwY2I0MTBkLTgxYjUtNDY5Yy04YjI4LTFiMWRmNzlkYThiMSIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cHM6Ly9taW5oYXNlbXByZXNhc2hvbS5hcHBzLm9jcC5yaW8uZ292LmJyIiwiaHR0cDovL2xvY2FsaG9zdDo4MDgwIiwiaHR0cHM6Ly9taW5oYXNlbXByZXNhc2Rldi5hcHBzLm9jcC5yaW8uZ292LmJyIiwiaHR0cHM6Ly9hcGktbWluaGFzZW1wcmVzYXNob20uYXBwcy5vY3AucmlvLmdvdi5iciIsImh0dHBzOi8vYXBpLW1pbmhhc2VtcHJlc2FzZGV2LmFwcHMub2NwLnJpby5nb3YuYnIiLCJodHRwOi8vbG9jYWxob3N0IiwiaHR0cDovL2xvY2FsaG9zdDozMDAwIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsImNhcmlvY2EtcmlvIiwidW1hX2F1dGhvcml6YXRpb24iLCJ1c2VyIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYnJva2VyIjp7InJvbGVzIjpbInJlYWQtdG9rZW4iXX0sImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwibmFtZSI6IkVEVUFSRE8gQU5UT05JTyBDQVNTQU5PIERFIFPDgSIsInByZWZlcnJlZF91c2VybmFtZSI6IjE2MjMyMzUwNzMxIiwiZ2l2ZW5fbmFtZSI6IkVEVUFSRE8iLCJmYW1pbHlfbmFtZSI6IkFOVE9OSU8gQ0FTU0FOTyBERSBTw4EiLCJlbWFpbCI6ImVkdWFyZG9jYXNzYW5vZGVzYUB1b2wuY29tLmJyIn0.sSBlMWysqOC-vMrgAqwLwjWBvTVvKShw9QJ7E08seUYd82Wh9aFNJ6KfypFh-pTdvl3SgvvRnYbXFXBM3wEvyWBN_wCr33rQfZL5Zrp-uvrLu07QTc-LU6gbj8GOqHa_5uZX_w69VW16J3hCfNoyXP5tXOTgpGiZZDppqa2KrbVE-incvFrwe69oF_igQ5XpZlfKfiV1t1_kyTtp1rFGWmaHDxc-mPAU7dYb6t5UByph_xOIduMAG1Kl-8hIKFekqAQv8d3V6LA556HMKsA6Ub0cHTkoL_ZM_ypa9ANAjNTNlVL7V4CyOSeWB4BG1Yy4ptPEDzEe422WpdGIUOl7BQ"
-      ),
+      obterCampanhasProprietario(),
   });
-
-  // const mutation = useMutation({
-  //   onSuccess: () => {
-  //     // Invalidate and refetch
-  //     queryClient.invalidateQueries({ queryKey: ["campaigns"] });
-  //   },
-  // });
-
-  console.log(query.data);
 
   return (
     <Table>
--- src/app/(services)/contracheque/page.tsx
+++ src/app/(services)/contracheque/page.tsx
@@ -2,61 +2,40 @@
 import { useCallback, useState } from "react";
 import { CampaignForm } from "./CampaignForm";
 import { CampaignsTable } from "./CampaignsTable";
+import { Campaign } from "@/utils/types";
+import { schema } from "@/schemas/campaign_form.schema";
+import { z } from "zod";
 
-const campaigns: Campaign[] = [
-  {
-    id: 1,
-    descricao: "Campanha 1",
-    // link: "https://example.com/campaign1",
-    urlImagem:
-      "https://upload.wikimedia.org/wikipedia/pt/a/ac/CRVascodaGama.png",
-    urlLink: "https://example.com/campaign1",
-    dataInicial: new Date("2024-01-01"),
-    dataFinal: new Date("2024-12-31"),
-    status: true,
-    cpfProprietario: "02928257730",
-    nomeCampanha: "Campanha 1",
-  },
-  {
-    id: 2,
-    descricao: "Campanha 2",
-    // link: "https://example.com/campaign2",
-    urlImagem:
-      "https://cdn.vox-cdn.com/thumbor/taY_jiqjaZKZhXNra4x0RIrZgxo=/0x0:3801x2534/1200x800/filters:focal(1597x963:2205x1571)/cdn.vox-cdn.com/uploads/chorus_image/image/73933993/2200643906.0.jpg",
-    urlLink: "https://example.com/campaign2",
-    dataInicial: new Date("2024-02-01"),
-    dataFinal: new Date("2024-03-31"),
-    status: false,
-    cpfProprietario: "02928257730",
-    nomeCampanha: "Campanha 2",
-  },
-];
+// const campaigns: Campaign[] = [
+//   {
+//     id: 1,
+//     descricao: "Campanha 1",
+//     // link: "https://example.com/campaign1",
+//     urlImagem:
+//       "https://upload.wikimedia.org/wikipedia/pt/a/ac/CRVascodaGama.png",
+//     urlLink: "https://example.com/campaign1",
+//     dataInicial: new Date("2024-01-01"),
+//     dataFinal: new Date("2024-12-31"),
+//     status: true,
+//     cpfProprietario: "02928257730",
+//     nomeCampanha: "Campanha 1",
+//   },
+//   {
+//     id: 2,
+//     descricao: "Campanha 2",
+//     // link: "https://example.com/campaign2",
+//     urlImagem:
+//       "https://cdn.vox-cdn.com/thumbor/taY_jiqjaZKZhXNra4x0RIrZgxo=/0x0:3801x2534/1200x800/filters:focal(1597x963:2205x1571)/cdn.vox-cdn.com/uploads/chorus_image/image/73933993/2200643906.0.jpg",
+//     urlLink: "https://example.com/campaign2",
+//     dataInicial: new Date("2024-02-01"),
+//     dataFinal: new Date("2024-03-31"),
+//     status: false,
+//     cpfProprietario: "02928257730",
+//     nomeCampanha: "Campanha 2",
+//   },
+// ];
 
-export type FormData = {
-  title: string;
-  verticalBanner: string;
-  horizontalBanner: string;
-  link: string;
-  description: string;
-  status: "active" | "inactive" | "draft";
-  startDate: string;
-  endDate: string;
-  startDateOpen: boolean;
-  endDateOpen: boolean;
-};
-
-export type Campaign = {
-  id: number;
-  descricao: string;
-  // link: string;
-  urlImagem: string;
-  urlLink: string;
-  dataInicial: Date;
-  dataFinal: Date;
-  status: boolean;
-  cpfProprietario: string;
-  nomeCampanha: string;
-} | null;
+export type FormData = z.infer<typeof schema>;
 
 export default function Contracheque() {
   const [defaultCampaign, setDefaultCampaign] = useState<Campaign | null>(null);
@@ -87,7 +66,6 @@ export default function Contracheque() {
               Lista de Campanhas
             </h2>
             <CampaignsTable
-              campaigns={campaigns}
               handleSetDefaultCampaign={handleSetDefaultCampaign}
             />
           </div>
