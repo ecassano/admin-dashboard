4b0795d2cd83018672c80d363938d01128128c34
eduardosa54 <eduardo.sa@rio.rj.gov.br>
2025-06-25 17:00:36 -0300

    feat: Adicionar type e dto para campaign

---
+++ src/api/dtos/campaign.dto.ts
@@ -0,0 +1,10 @@
+export type CampaignDto = {
+  descricao: string
+  urlImagem: string
+  urlLink: string | null | undefined
+  dataInicial: string
+  dataFinal: string
+  status: boolean
+  cpfProprietario: string
+  nomeCampanha: string
+}
---
+++ src/utils/types.d.ts
@@ -0,0 +1,11 @@
+export type Campaign = {
+  id: number
+  descricao: string
+  urlImagem: string
+  urlLink: string
+  dataInicial: string
+  dataFinal: string
+  status: boolean
+  cpfProprietario: string
+  nomeCampanha: string
+} | null
